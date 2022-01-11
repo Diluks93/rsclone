db7c49f3743f83621464c30b873aa30262cc5fda
jenya-b <evgeny_rab@inbox.ru>
2022-01-11 19:03:24 +0300

    feat: add episode list

---
+++ rsclone/src/modules/core/interfaces/enums.ts
@@ -0,0 +1,20 @@
+const enum PageIds {
+  MainPage = 'main-page',
+  HomePage = 'home-page',
+  SettingsPage = 'settings-page',
+  AuthorsPage = 'authors-page',
+  ErrorPage = 'error-page',
+  CurrentPage = 'current-page',
+  CategoriesPage = 'categories-page',
+}
+
+const enum ErrorTypes {
+  Error_404 = '404',
+}
+
+const enum LevelsSeries {
+  preparatoryLevel = 3,
+  mainLevel = 4,
+}
+
+export { PageIds, ErrorTypes, LevelsSeries };
