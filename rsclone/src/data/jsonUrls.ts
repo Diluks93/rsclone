f2e4ab32302ba482743b8f0819cb46a457f83754
juneagain <juneagain@mail.ru>
2022-01-11 13:51:05 +0300

    refactor: extract json urls to separate file

---
+++ rsclone/src/data/jsonUrls.ts
@@ -0,0 +1,11 @@
+export const homeJsonUrl =
+  'https://raw.githubusercontent.com/Diluks93/source-rsclone/new-files/rsclone-source/json/home.json';
+
+export const categoriesJsonUrl =
+  'https://raw.githubusercontent.com/Diluks93/source-rsclone/new-files/rsclone-source/json/categories.json';
+
+export const episodesJsonUrl =
+  'https://raw.githubusercontent.com/Diluks93/source-rsclone/new-files/rsclone-source/json/episodes.json';
+
+export const settingsJsonUrl =
+  'https://raw.githubusercontent.com/Diluks93/source-rsclone/new-files/rsclone-source/json/settings.json';
