ae886fab950e46bf2f23188c6e681545e9144e34
juneagain <juneagain@mail.ru>
2022-01-11 13:50:54 +0300

    refactor: extract elements props to separate file

---
+++ rsclone/src/constants/elementsProps.ts
@@ -0,0 +1,38 @@
+export const rangesProps: Record<string, string>[] = [
+  {
+    iconUrl:
+      'https://raw.githubusercontent.com/randomspells/source-rsclone/af3870fdc4d1d92e27d7603277d7c09b9710b449/rsclone-source/settings-page/svg/sound.svg',
+    id: 'volume-bar',
+    value: '60',
+  },
+];
+
+export const checkboxesProps: Record<string, string>[] = [
+  {
+    text: 'Вкл / откл звук',
+    id: 'toggle-sound',
+  },
+  {
+    text: 'Ограничение по времени',
+    id: 'toggle-time',
+  },
+  {
+    text: 'Отчет по пакостям',
+    id: 'toggle-report',
+  },
+];
+
+export const btnsProps: Record<string, string>[] = [
+  {
+    text: 'Удалить прогресс игры',
+    id: 'reset-progress',
+  },
+  {
+    text: 'Отмена',
+    id: 'cancel-settings',
+  },
+  {
+    text: 'Ок',
+    id: 'save-settings',
+  },
+];
