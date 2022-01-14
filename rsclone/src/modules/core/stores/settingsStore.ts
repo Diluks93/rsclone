04125843e77acbedb3d65a721c36c96241c7cebf
juneagain <juneagain@mail.ru>
2022-01-14 20:38:31 +0300

    feat: implement settings store with state change on events and translation fetch method

---
+++ rsclone/src/modules/core/stores/settingsStore.ts
@@ -0,0 +1,111 @@
+import { jsonUrl } from './../data/jsonUrls';
+import { SettingsTranslationKeys, StorageKeys } from '../interfaces/enums';
+import { SettingsConfigType } from './../types/settingsTypes';
+import { LanguageKeys } from './translationStore';
+
+const defaultConfig: SettingsConfigType = {
+  languageValue: 'ru',
+  volumeValue: '0.5',
+  isSoundEnabled: true,
+  isTimeLimitEnabled: false,
+  isTricksReportEnabled: false,
+};
+
+class SettingsStore {
+  private _languageValue: LanguageKeys;
+
+  private _volumeValue: string;
+
+  private _isSoundEnabled: boolean;
+
+  private _isTimeLimitEnabled: boolean;
+
+  private _isTricksReportEnabled: boolean;
+
+  constructor({
+    languageValue,
+    volumeValue,
+    isSoundEnabled,
+    isTimeLimitEnabled,
+    isTricksReportEnabled,
+  }: SettingsConfigType) {
+    this._languageValue = languageValue;
+    this._volumeValue = volumeValue;
+    this._isSoundEnabled = isSoundEnabled;
+    this._isTimeLimitEnabled = isTimeLimitEnabled;
+    this._isTricksReportEnabled = isTricksReportEnabled;
+  }
+
+  get languageValue(): LanguageKeys {
+    return this._languageValue;
+  }
+
+  set languageValue(value: LanguageKeys) {
+    this._languageValue = value;
+  }
+
+  get volumeValue(): string {
+    return this._volumeValue;
+  }
+
+  set volumeValue(value: string) {
+    this._volumeValue = value;
+  }
+
+  get isSoundEnabled(): boolean {
+    return this._isSoundEnabled;
+  }
+
+  set isSoundEnabled(value: boolean) {
+    this._isSoundEnabled = value;
+  }
+
+  get isTimeLimitEnabled(): boolean {
+    return this._isTimeLimitEnabled;
+  }
+
+  set isTimeLimitEnabled(value: boolean) {
+    this._isTimeLimitEnabled = value;
+  }
+
+  get isTricksReportEnabled(): boolean {
+    return this._isTricksReportEnabled;
+  }
+
+  set isTricksReportEnabled(value: boolean) {
+    this._isTricksReportEnabled = value;
+  }
+
+  private async getSpecificPhrase(lang: LanguageKeys, key: string) {
+    const data = await fetch(jsonUrl[StorageKeys.SettingsTranslation]);
+    const json = await data.json();
+    return json[lang][key];
+  }
+
+  setSettingsLanguage() {
+    const language = this.languageValue;
+    const translationKeys = Object.values(SettingsTranslationKeys);
+    translationKeys.forEach((key) => {
+      let element = document.getElementById(key);
+      if (!(element instanceof HTMLInputElement)) {
+        this.getSpecificPhrase(language, key).then((value) => {
+          if (element !== null) {
+            element.textContent = value;
+          }
+        });
+      } else {
+        element = element.parentElement;
+        if (element === null) return;
+        element.childNodes.forEach((node) => {
+          if (node.nodeType === 3) {
+            this.getSpecificPhrase(language, key).then((phrase) => {
+              node.textContent = phrase;
+            });
+          }
+        });
+      }
+    });
+  }
+}
+
+export const settingsStore = new SettingsStore(defaultConfig);
