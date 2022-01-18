a4dd877f116c4440faef60c303975cef9c21829d
juneagain <juneagain@mail.ru>
2022-01-18 22:35:16 +0300

    feat: rework home page

--- rsclone/src/modules/pages/home/index.ts
+++ rsclone/src/modules/pages/home/index.ts
@@ -1,20 +1,50 @@
-import { homeTitleProps, homeLinkButtonsProps } from './../../core/constants/constHome';
+import { homeTitleProps, homeLinkButtonProps } from './../../core/constants/constHome';
 import Page from '../../core/templates/Page';
 import './style.scss';
+import { GameTranslationInterface, LanguageKeys } from '../../core/types/types';
 
 class HomePage extends Page {
-  renderWrapper() {
-    const wrapper = document.createElement('div');
-    wrapper.classList.add('home-page__wrapper');
+  homeButtonsWrapper: HTMLDivElement;
 
-    this.createLinkButtons(homeLinkButtonsProps).forEach((item) => wrapper.append(item));
-    this.container.append(wrapper);
+  gameTitle: HTMLElement;
+
+  startGameButton: HTMLAnchorElement;
+
+  openSettingsButton: HTMLAnchorElement;
+
+  openAuthorsButton: HTMLAnchorElement;
+
+  constructor(id: string, className: string) {
+    super(id, className);
+    this.gameTitle = this.createHeaderTitle(homeTitleProps);
+    this.startGameButton = this.createLinkButton(homeLinkButtonProps.startGameButton);
+    this.openSettingsButton = this.createLinkButton(homeLinkButtonProps.openSettingsButton);
+    this.openAuthorsButton = this.createLinkButton(homeLinkButtonProps.openAuthorsButton);
+    this.homeButtonsWrapper = this.createWrapper('home-page__wrapper');
+  }
+
+  setPageLanguage(translation: GameTranslationInterface, lang: LanguageKeys) {
+    this.gameTitle.style.backgroundImage = `url(${translation[lang].gameTitle})`;
+    this.startGameButton.textContent = translation[lang].startGameButton;
+    this.openSettingsButton.textContent = translation[lang].openSettingsButton;
+    this.openAuthorsButton.textContent = translation[lang].openAuthorsButton;
+  }
+
+  createWrapper(className: string): HTMLDivElement {
+    const wrapper = document.createElement('div');
+    wrapper.classList.add(className);
+
+    wrapper.append(this.startGameButton);
+    wrapper.append(this.openSettingsButton);
+    wrapper.append(this.openAuthorsButton);
+
+    return wrapper;
   }
 
   render() {
-    const title = this.createHeaderTitle(homeTitleProps);
-    this.container.append(title);
-    this.renderWrapper();
+    this.container.append(this.gameTitle);
+    this.container.append(this.homeButtonsWrapper);
+
     return this.container;
   }
 }
