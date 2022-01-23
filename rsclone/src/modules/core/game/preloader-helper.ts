b6b4c12fd5a5219f231aa1f441d72756a964cc77
Dzmitry Ilyukovich <mr.ilyukovich@gmail.com>
2022-01-23 20:51:24 +0300

    feat: add camera follow

--- rsclone/src/modules/core/game/preloader-helper.ts
+++ rsclone/src/modules/core/game/preloader-helper.ts
@@ -1,25 +1,13 @@
-import GameScene from "./game-scene";
-
-const DUDE_KEY = 'dude';
+import GameScene from './game-scene';
+import { GameKeys, Urls } from '../enums/enums';
 
 export default class PreloadHelper {
   static preload(scene: GameScene): void {
-    scene.load.setBaseURL('https://raw.githubusercontent.com/Diluks93/source-rsclone/main/rsclone-source');
-    scene.load.image('assets', 'game/map/assets.png');
-    scene.load.tilemapTiledJSON('map', 'game/map/map-test.json');
+    scene.load.setBaseURL(Urls.main);
+    scene.load.image(GameKeys.ASSETS, Urls.assets);
+    scene.load.tilemapTiledJSON(GameKeys.MAP, Urls.map);
     
-    scene.load.spritesheet(DUDE_KEY, 'game/voody.png', { frameWidth: 190, frameHeight: 257 });
+    scene.load.spritesheet(GameKeys.DUDE_KEY, Urls.voody, { frameWidth: 190, frameHeight: 257 });
   }
-
-    // preload() {
-  //   this.load.setBaseURL('https://raw.githubusercontent.com/Diluks93/source-rsclone/main/rsclone-source');
-
-  //   this.load.image('assets', 'game/map/assets.png');
-  //   this.load.tilemapTiledJSON('map', 'game/map/map..json');
-    
-  //   // this.load.image('bricks', 'game/bricks-wall.png');
-  //   // this.load.image(WALL_KEY, 'game/wall.png');
-  //   this.load.spritesheet(DUDE_KEY, 'game/voody.png', { frameWidth: 190, frameHeight: 257 });
-  // }
 
 }
