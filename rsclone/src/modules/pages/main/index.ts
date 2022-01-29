cc1f5bd195bec953dd8c6454468509511f08f8bf
juneagain <juneagain@mail.ru>
2022-01-29 15:43:09 +0300

    feat: add close game button to main-page

--- rsclone/src/modules/pages/main/index.ts
+++ rsclone/src/modules/pages/main/index.ts
@@ -3,12 +3,33 @@ import { config } from '../../core/game/game';
 import './style.scss';
 
 class MainPage extends Page {
+  closeGameButton: HTMLElement;
+
   static TextObject = {
     mainTitle: 'Main Page',
   };
 
+  constructor(id: string, className: string) {
+    super(id, className);
+    this.closeGameButton = this.createCloseGameButton();
+  }
+
+  private createCloseGameButton(): HTMLElement {
+    const linkButton = document.createElement('a');
+    linkButton.classList.add('primary-button', 'main-page__button', 'basic-hover');
+    linkButton.href = '#home-page';
+    linkButton.textContent = 'x';
+
+    linkButton.addEventListener('click', () => {
+      const canvasParent = document.getElementById('first-step');
+      canvasParent?.classList.add('hidden');
+    });
+    return linkButton;
+  }
+
   render() {
     new Phaser.Game(config);
+    this.container.append(this.closeGameButton);
     return this.container;
   }
 }
