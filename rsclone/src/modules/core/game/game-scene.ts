b5e874e6fa8a86f9aee60229163d1c062a2edd7a
juneagain <juneagain@mail.ru>
2022-02-06 22:07:13 +0300

    feat: add classes for 2 types of items, add example items, add metods to detect and handle overlap

--- rsclone/src/modules/core/game/game-scene.ts
+++ rsclone/src/modules/core/game/game-scene.ts
@@ -3,9 +3,14 @@ import Player from './player';
 
 import { config } from './game';
 import { GameKey, SceneKey } from '../enums/enums';
+import TrickSourceItem from './trick-source-item';
+import TrickTargetItem from './trick-target-item';
+import { TargetItemConfigType } from '../types/types';
 
 export default class GameScene extends Phaser.Scene {
   cursor: ReturnType<<T>() => T>;
+
+  actionKey: Phaser.Input.Keyboard.Key | undefined;
 
   player: Player;
 
@@ -17,12 +22,15 @@ export default class GameScene extends Phaser.Scene {
 
   height = config.scale.height;
 
-  pen: Phaser.GameObjects.Image | undefined;
+  trickSourceItems: TrickSourceItem[] = [];
+
+  trickTargetItems: TrickTargetItem[] = [];
+
+  isActionAvailable = false;
 
   sizeWorld = {
     width: 3840,
     height: 1536,
-    // heightFloor: 1000,
   };
 
   mapLayer = {
@@ -39,13 +47,13 @@ export default class GameScene extends Phaser.Scene {
 
   constructor() {
     super({ key: SceneKey.FirstStep });
-    this.player = new Player();
-    this.music = undefined;
     this.playerSounds = {};
+    this.player = new Player(this, 0, 0, this.playerSounds);
   }
 
   create(): void {
-    // this.add.image(0, 0, 'bricks').setOrigin(0, 0).setScale(1);
+    this.actionKey = this.input.keyboard.addKey('E');
+
     const map = this.make.tilemap({ key: 'map', tileWidth: 32, tileHeight: 32 });
     const tileset = map.addTilesetImage('assets', 'assets');
     this.playerSounds.footsteps = this.sound.add(GameKey.SoundFootsteps);
@@ -56,6 +64,21 @@ export default class GameScene extends Phaser.Scene {
 
     this.platforms = map.createLayer(this.mapLayer.platforms, tileset, 0, 0);
     this.platforms.setCollisionByExclusion([-1], true);
+
+    const picture = this.createTrickTargetItem({
+      x: 3000,
+      y: 1200,
+      originalItemKey: GameKey.Picture,
+      trickedItemKey: GameKey.TrickedPicture,
+      actionItemKey: GameKey.Pen,
+    });
+    this.trickTargetItems.push(picture);
+    console.log(picture.originalItem.getBounds());
+
+    const pen = new TrickSourceItem(this, 3200, 1300, GameKey.Pen);
+    const pen2 = new TrickSourceItem(this, 3300, 1300, GameKey.Pen);
+    this.trickSourceItems.push(pen);
+    this.trickSourceItems.push(pen2);
 
     const spawnPoint = map.findObject(this.mapLayer.object.id, (obj) => obj.name === this.mapLayer.object.name);
 
@@ -77,11 +100,7 @@ export default class GameScene extends Phaser.Scene {
       this.physics.add.collider(this.player.sprite, this.platforms);
     }
 
-    if (spawnPoint.x && spawnPoint.y) {
-      this.pen = this.add.image(spawnPoint.x - 1000, spawnPoint.y, GameKey.Pen);
-      this.physics.add.existing(this.pen);
-      this.physics.add.collider(this.pen, this.platforms);
-    }
+    this.addOverlapActionToItems();
 
     this.cursor = this.input.keyboard.createCursorKeys();
     this.scale.on('resize', this.resize, this);
@@ -93,14 +112,82 @@ export default class GameScene extends Phaser.Scene {
 
   update(): void {
     this.player.update();
+
+    // "E" label toggle process
+    if (
+      this.isPlayerOverlapItems(this.trickSourceItems) ||
+      (this.isPlayerOverlapItems(this.trickTargetItems) && this.isActionAvailable)
+    ) {
+      this.player.actionLabel?.setVisible(true);
+    } else {
+      this.player.actionLabel?.setVisible(false);
+    }
   }
 
   resize(gameSize: Record<string, number>): void {
     const width = gameSize.width;
     const height = gameSize.height;
-
     this.cameras.resize(width, height);
-
     this.platforms?.setSize(width, height);
+  }
+
+  createTrickTargetItem(itemConfig: TargetItemConfigType): TrickTargetItem {
+    const { x, y, originalItemKey, trickedItemKey, actionItemKey } = itemConfig;
+    const originalItem = this.add.image(0, 0, originalItemKey);
+    const trickedItem = this.add.image(0, 0, trickedItemKey);
+    return new TrickTargetItem(this, x, y, [originalItem, trickedItem], actionItemKey);
+  }
+
+  isPlayerOverlapItems(items: TrickSourceItem[] | TrickTargetItem[]): boolean {
+    const playerBounds = this.player.sprite!.getBounds();
+    const itemsBounds = items.map((item) => item.getBounds());
+    return itemsBounds.some((itemBound) => {
+      return Phaser.Geom.Intersects.RectangleToRectangle(playerBounds, itemBound);
+    });
+  }
+
+  applyItem(targetItem: TrickTargetItem) {
+    this.player.removeItem(targetItem.keyItemId);
+    targetItem.trickedItem.setVisible(true);
+    targetItem.isTricked = true;
+    this.player.isPerformTrick = false;
+  }
+
+  pickUpItem(sourceItem: Phaser.GameObjects.Image) {
+    this.player.addItem(sourceItem.texture.key);
+    this.trickSourceItems = this.trickSourceItems.filter((filteredItem) => filteredItem !== sourceItem);
+    sourceItem!.destroy();
+    this.player.actionLabel?.setVisible(false);
+  }
+
+  addOverlapActionToItems() {
+    this.trickSourceItems.forEach((sourceItem) => {
+      this.physics.add.collider(sourceItem, this.platforms!);
+      this.physics.add.overlap(this.player.sprite!, sourceItem, () => {
+        if (this.actionKey!.isDown) {
+          this.pickUpItem(sourceItem);
+        }
+      });
+    });
+
+    this.trickTargetItems.forEach((targetItem) => {
+      this.physics.add.existing(targetItem, true);
+      this.physics.add.overlap(this.player.sprite!, targetItem.originalItem, () => {
+        this.isActionAvailable = this.player.inventory.includes(targetItem.keyItemId);
+        if (this.isActionAvailable && this.actionKey!.isDown) {
+          this.player.isPerformTrick = true;
+          this.player.playerSounds?.prank.play();
+          this.time.addEvent({
+            delay: 1000,
+            callback: () => {
+              this.applyItem(targetItem);
+            },
+          });
+        }
+        if (!this.isActionAvailable) {
+          this.player.actionLabel?.setVisible(false);
+        }
+      });
+    });
   }
 }
